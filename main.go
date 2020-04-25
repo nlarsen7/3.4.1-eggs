40942fd409033d2156677f4af7eb064a18af8352
nlarsen7 <nlarsen2013@gmail.com>
2020-04-25 20:59:29 +0000

    3.4.1 eggs

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/25/2020
+//picks a random number and outputs the random number
 package main
 
 import (
@@ -8,8 +11,11 @@ import (
 func dozens() {
 rand.Seed(time.Now().UnixNano())
 eggs:=(rand.Intn(12)+1)
+//random number of eggs in a dozen
 fmt.Println("There are",eggs,"amount of eggs in this dozen")
+//Prints out the amount of eggs
 }
 func main() {
   dozens()
+  //outputs what the Dozen function does
 }
